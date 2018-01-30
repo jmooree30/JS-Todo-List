2eeb3d88e9fa9e8f6b5681c29e17e8759d670216
Jacob <jmooree30@gmail.com>
2018-01-29 22:07:40 -0500

    Initial dist subtree commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -67,7 +67,25 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log("hey")
+const Newtodo = (title,description,date,priority) => {
+  return {
+   title,
+   description,
+   date,
+   priority,
+  }
+}
+
+  document.querySelector("#new-project").addEventListener("click",function(){
+    document.querySelector(".modal").style.display = "inline";
+    document.querySelector("#page-mask").style.display = "inline";
+  });
+
+  document.querySelector("#close").addEventListener("click",function(){
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector("#page-mask").style.display = "none";
+  });
+
 
 /***/ })
 /******/ ]);
