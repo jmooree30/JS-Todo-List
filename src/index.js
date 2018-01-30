27684024e5f10895c39dbcf5128fb921133760cd
Jacob <jmooree30@gmail.com>
2018-01-29 22:08:08 -0500

    modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hey")
+const Newtodo = (title,description,date,priority) => {
+  return {
+   title,
+   description,
+   date,
+   priority,
+  }
+}
+
+  document.querySelector("#new-project").addEventListener("click",function(){
+    document.querySelector(".modal").style.display = "inline";
+    document.querySelector("#page-mask").style.display = "inline";
+  });
+
+  document.querySelector("#close").addEventListener("click",function(){
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector("#page-mask").style.display = "none";
+  });
