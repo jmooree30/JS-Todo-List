e69a04df3155c272e32497d779bc8689639b9865
Jacob <jmooree30@gmail.com>
2018-01-30 23:53:44 -0500

    todo modal started

--- src/index.js
+++ src/index.js
@@ -1,42 +1,74 @@
 const projectArr = [];
+let projectCounter = 0;
 
 const Newtodo = (title,date,priority) => {
+  const dataset = projectCounter
   return {
+   dataset,
    title,
    date,
    priority,
-  }
+ }
 };
 
 function populateProjects(){
-    div = document.createElement("div")
-    div.innerHTML = projectArr[projectArr.length - 1].title
-    div.classList = "eachproject"
-    div.style.color = projectArr[projectArr.length - 1].priority
-    span = document.createElement("span")
-    span.innerHTML = projectArr[projectArr.length - 1].date
-    span.style.float = "right"
-    document.querySelector(".projects").appendChild(div).appendChild(span)
+  div = document.createElement("div")
+  div.classList = "eachproject"
+  button = document.createElement("button")
+  button.classList = "list-button"
+  button.innerHTML = projectArr[projectArr.length - 1].title
+  button.dataset.name = projectArr[projectArr.length - 1].dataset
+  button.style.background = projectArr[projectArr.length - 1].priority
+  span = document.createElement("span")
+  span.innerHTML = projectArr[projectArr.length - 1].date
+  span.style.float = "right"
+  document.querySelector(".projects").appendChild(div).appendChild(button)
+  document.querySelector(".projects").appendChild(div).appendChild(span)
 }
 
-  document.querySelector("#new-project").addEventListener("click",function(){
-    document.querySelector(".modal").style.display = "inline";
+//brings up new project modal
+document.querySelector("#new-project").addEventListener("click",function(){
+  document.querySelector(".modal").style.display = "inline";
+  document.querySelector("#page-mask").style.display = "inline";
+});
+
+//close button inside modal
+document.querySelector("#close").addEventListener("click",function(){
+  document.querySelector(".modal").style.display = "none";
+  document.querySelector("#page-mask").style.display = "none";
+});
+
+//creates project and closes modal
+document.querySelector("form").addEventListener("submit", function(e){
+  values = document.querySelector("form")
+  document.querySelector(".modal").style.display = "none";
+  document.querySelector("#page-mask").style.display = "none";
+  e.preventDefault()
+  const newProject = Newtodo(values.elements["title"].value,
+   values.elements["date"].value,
+   values.elements["priority"].value)
+  projectArr.push(newProject)
+  populateProjects()
+  projectCounter += 1;
+  todoList()
+})
+
+//opens todo list 
+function todoList(){
+listButton = document.querySelectorAll(".list-button")
+listButton.forEach(function(e){
+  e.addEventListener("click",function(d){
     document.querySelector("#page-mask").style.display = "inline";
-  });
-
-  document.querySelector("#close").addEventListener("click",function(){
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector("#page-mask").style.display = "none";
-  });
-
-  document.querySelector("form").addEventListener("submit", function(e){
-    values = document.querySelector("form")
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector("#page-mask").style.display = "none";
-    e.preventDefault()
-    const newProject = Newtodo(values.elements["title"].value,
-                               values.elements["date"].value,
-                               values.elements["priority"].value)
-    projectArr.push(newProject)
-    populateProjects()
+    document.querySelector(".modal-2").style.display = "inline";
+    findDataset(e.dataset.name)
   })
+})
+}
+
+//finds the project object relevant to project clicked.
+function findDataset(e){
+  projectArr.forEach(function(obj){
+    if(obj.dataset == e)
+      console.log(obj)
+  })
+}
