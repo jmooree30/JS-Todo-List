5b59439c56689e9269b35bf274680a4754f2e79f
Jacob <jmooree30@gmail.com>
2018-01-30 21:15:40 -0500

    project factory complete

--- bundle.js
+++ bundle.js
@@ -67,14 +67,26 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-const Newtodo = (title,description,date,priority) => {
+const projectArr = [];
+
+const Newtodo = (title,date,priority) => {
   return {
    title,
-   description,
    date,
    priority,
   }
 };
+
+function populateProjects(){
+    div = document.createElement("div")
+    div.innerHTML = projectArr[projectArr.length - 1].title
+    div.classList = "eachproject"
+    div.style.color = projectArr[projectArr.length - 1].priority
+    span = document.createElement("span")
+    span.innerHTML = projectArr[projectArr.length - 1].date
+    span.style.float = "right"
+    document.querySelector(".projects").appendChild(div).appendChild(span)
+}
 
   document.querySelector("#new-project").addEventListener("click",function(){
     document.querySelector(".modal").style.display = "inline";
@@ -85,6 +97,18 @@ const Newtodo = (title,description,date,priority) => {
     document.querySelector(".modal").style.display = "none";
     document.querySelector("#page-mask").style.display = "none";
   });
+
+  document.querySelector("form").addEventListener("submit", function(e){
+    values = document.querySelector("form")
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector("#page-mask").style.display = "none";
+    e.preventDefault()
+    const newProject = Newtodo(values.elements["title"].value,
+                               values.elements["date"].value,
+                               values.elements["priority"].value)
+    projectArr.push(newProject)
+    populateProjects()
+  })
 
 
 /***/ })
